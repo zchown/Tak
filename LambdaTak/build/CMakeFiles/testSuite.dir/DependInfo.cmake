07f03d909c6cd1d88437ddc14809fda038013960
zchown <91749140+zchown@users.noreply.github.com>
2025-02-14 16:55:47 -0500

    git cleanup

---
+++ LambdaTak/build/CMakeFiles/testSuite.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/zanderchown/ComputerScience/Tak/LambdaTak/src/board.c" "CMakeFiles/testSuite.dir/src/board.c.o" "gcc" "CMakeFiles/testSuite.dir/src/board.c.o.d"
+  "/Users/zanderchown/ComputerScience/Tak/LambdaTak/tests/testBoard.c" "CMakeFiles/testSuite.dir/tests/testBoard.c.o" "gcc" "CMakeFiles/testSuite.dir/tests/testBoard.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ LambdaTak/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/zanderchown/ComputerScience/Tak/LambdaTak
+# Build directory: /Users/zanderchown/ComputerScience/Tak/LambdaTak/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(BoardTests "/Users/zanderchown/ComputerScience/Tak/LambdaTak/build/testSuite")
+set_tests_properties(BoardTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/zanderchown/ComputerScience/Tak/LambdaTak/CMakeLists.txt;25;add_test;/Users/zanderchown/ComputerScience/Tak/LambdaTak/CMakeLists.txt;0;")
